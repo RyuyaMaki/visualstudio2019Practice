dd1ad909514c291b37d7fcd57941d9e28868ba36
Ryuya Maki <ryuyan1030@gmail.com>
2020-07-17 12:05:30 +0900

    remake main.cpp

--- visualstudio2019_Sample/project1/main.cpp
+++ visualstudio2019_Sample/project1/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
 
+#include "GameObjectManager/GameObjectManager.h"
+#include "GameObject/GameObject.h"
+
 int main() {
 	std::string order;
+	GameObjectManager m_gameobjectmaneger;
+
+	m_gameobjectmaneger.add(new GameObject("GameObjectA"));
+	m_gameobjectmaneger.add(new GameObject("GameObjectB"));
 
 	std::cout << "プログラム開始" << std::endl;
 
@@ -12,10 +19,18 @@ int main() {
 		if (order == "a") {
 			std::cout << "フレーム更新" << std::endl;
 			// updateとdrawを行う
+			m_gameobjectmaneger.update();
+			m_gameobjectmaneger.draw();
 		}
 		else if (order == "add") {
-			std::cout << "オブジェクトを追加" << std::endl;
+			std::cout << "オブジェクトを追加します。\n" << "名前を入力してください" << std::endl;
+			std::string gameObjectName;
+			std::cin >> gameObjectName;
+
 			// オブジェクトを追加したのちupdateとdrawを行う
+			m_gameobjectmaneger.add(new GameObject(gameObjectName));
+			m_gameobjectmaneger.update();
+			m_gameobjectmaneger.draw();
 		}
 		else if (order == "end") {
 			std::cout << "プログラム終了" << std::endl;
