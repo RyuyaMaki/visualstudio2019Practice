47418a1bddcde60d21d145e56d7433d9a03503d6
Ryuya Maki <ryuyan1030@gmail.com>
2020-07-21 09:56:27 +0900

    fixed ComponentManager

--- visualstudio2019_Sample/project1/ComponentManager/ComponentManager.cpp
+++ visualstudio2019_Sample/project1/ComponentManager/ComponentManager.cpp
@@ -1,21 +1,20 @@
 #include "ComponentManager.h"
 #include "../Component/Component.h"
 
-void ComponentManager::add(std::shared_ptr<Component> component)
-{
+std::list<std::shared_ptr<Component>> ComponentManager::m_components;
+
+void ComponentManager::add(std::shared_ptr<Component> component){
 	m_components.push_back(component);
 }
 
-void ComponentManager::update()
-{
-	for (auto component : m_components) {
+void ComponentManager::update(){
+	for (const auto& component : m_components) {
 		component->update();
 	}
 }
 
-void ComponentManager::draw()
-{
-	for (auto component : m_components) {
+void ComponentManager::draw(){
+	for (const auto& component : m_components) {
 		component->draw();
 	}
 }
